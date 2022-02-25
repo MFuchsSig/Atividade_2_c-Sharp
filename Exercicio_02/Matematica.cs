76fbcb67dfa6699da1362069b3e6b1040225e4e4
Matheus Fuchs Signorini <matheusfuchssig@hotmail.com>
2022-02-25 09:58:19 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_02/Matematica.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_02
+{
+    class Matematica
+    {
+        public static int exponenciacao(int Base, int Exp)
+        {
+            int Resultado = Base;
+
+            for(int i =1; i < Exp; i++)
+            {
+                Resultado = Resultado * Base;
+            }
+                    
+            return Resultado;
+        }
+
+
+    }
+}
---
+++ Exercicio_02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Resultado = Matematica.exponenciacao(2, 4);
+
+            Console.WriteLine("a exponenciação de 2 elevado 4 é " + Resultado );
+
+            Console.ReadKey();
+        }
+    }
+}
